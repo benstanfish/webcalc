48eeb3ccec0f27b4a49ea1b3e9a17c4887eb5215
benstanfish <benstanfish@hotmail.com>
2023-10-06 14:49:09 -0700

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 'use strict';
 let stack = [];
+let expFlag = false;
 
 document.onkeyup = (keyUpEvent) => {
   const readout = document.getElementById('readout');
@@ -21,13 +22,21 @@ document.onkeyup = (keyUpEvent) => {
       break;
     case 'Delete':
       readout.value = '';
+      expFlag = false;
+      break;
+    case 'ArrowDown':
+      pull_from_stack();
       break;
     case 'e':
-      test_for_exp();
+      toggle_eex();
       break;
     case 'd':
     case 'Backspace':
+    case 'ArrowLeft':
       readout.value = readout.value.slice(0, -1);
+      if (readout.value.length <= 0 && expFlag === true) {
+        toggle_eex();
+      }
       break;
     case '+':
       // if (readout.value.length !== 0) add_to_history('+');
@@ -44,15 +53,63 @@ document.onkeyup = (keyUpEvent) => {
       break;
     case '=':
     case 'Enter':
-      if (readout.value != '') {
-        stack.unshift(readout.value);
-        add_stack();
-        readout.value = '';
-      }
+    case 'ArrowUp':
+      add_to_stack();
+      reset_eex();
       break;
     default:
   }
 };
+
+function clear_stack() {
+  stack = [];
+  for (let i = 0; i < 5; i++) {
+    let slot = document.getElementById(`stack${i}`);
+    slot.innerText = '';
+  }
+}
+
+function refresh_stack() {
+  for (let i = 0; i < 5; i++) {
+    const cell = document.getElementById(`stack${i}`);
+    cell.innerText = '';
+  }
+  if (stack.length > 0) {
+    for (let i = 0; i < stack.length; i++) {
+      const cell = document.getElementById(`stack${i}`);
+      cell.innerText = stack[i];
+    }
+  }
+}
+
+function add_to_stack() {
+  if (readout.value != '') {
+    stack.unshift(readout.value);
+    refresh_stack();
+    readout.value = '';
+    console.log(stack);
+  }
+}
+
+function pull_from_stack() {
+  let to_readout = stack.shift();
+  refresh_stack();
+  if (to_readout !== undefined) {
+    readout.value = to_readout;
+  } else {
+    readout.value = '';
+  }
+}
+
+function swap_xy() {
+  const readout = document.getElementById('readout');
+  const stack0 = document.getElementById('stack0');
+  readout.value.length === 0 ? (readout.value = 0) : readout.value;
+  stack0.innerText.length === 0 ? (stack0.innerText = 0) : stack0.innerText;
+  let temp = readout.value;
+  readout.value = stack0.innerText;
+  stack0.innerText = temp;
+}
 
 function test_for_leading_zero() {
   const readout = document.getElementById('readout');
@@ -69,11 +126,59 @@ function test_for_decimal() {
   const readout = document.getElementById('readout');
   if (readout.value.length === 0) {
     return false;
-  } else if (readout.value.indexOf('.') > 0) {
+  } else if (readout.value.indexOf('.') >= 0) {
     return false;
   } else {
     readout.value = readout.value += '.';
     return true;
+  }
+}
+
+function toggle_eex() {
+  const btn = document.getElementById('eex');
+  const readout = document.getElementById('readout');
+  if (readout.value.length > 0) {
+  } else {
+    readout.value = 1;
+  }
+  expFlag = !expFlag;
+  if (expFlag) {
+    btn.classList.add('warning');
+    btn.classList.remove('secondary');
+  } else {
+    btn.classList.add('secondary');
+    btn.classList.remove('warning');
+  }
+  if (readout.value.length > 0 && readout.value.indexOf('e') < 1) {
+    readout.value += 'e';
+  } else {
+  }
+}
+
+function reset_eex() {
+  expFlag = false;
+  const btn = document.getElementById('eex');
+  btn.classList.add('secondary');
+  btn.classList.remove('warning');
+}
+
+function change_sign() {
+  const readout = document.getElementById('readout');
+  if (expFlag) {
+    let vals = readout.value.split('e');
+    let pow = vals[1];
+    if (pow.indexOf('-') >= 0) {
+      pow = pow.replace('-', '');
+    } else {
+      pow = '-' + pow;
+    }
+    readout.value = vals[0] + 'e' + pow;
+  } else {
+    if (readout.value.indexOf('-') === 0) {
+      readout.value = readout.value.replace('-', '');
+    } else {
+      readout.value = '-' + readout.value;
+    }
   }
 }
 
@@ -88,31 +193,3 @@ function add_to_history(arg) {
     readout.value = '';
   }
 }
-
-function refresh_stack() {
-  if (stack.length > 0) {
-    for (let i = 0; i < stack.length; i++) {
-      let slot = document.getElementById(`stack${i}`);
-      slot.innerText = stack[i];
-    }
-  }
-}
-
-function add_stack() {
-  if (stack.length > 0) {
-    for (let i = 0; i < stack.length; i++) {
-      let slot = document.getElementById(`stack${i}`);
-      slot.innerText = stack[i];
-    }
-  }
-}
-
-function clear_stack() {
-  stack = [];
-  for (let i = 0; i < 5; i++) {
-    let slot = document.getElementById(`stack${i}`);
-    slot.innerText = '';
-  }
-}
-
-function swap_xy() {}
